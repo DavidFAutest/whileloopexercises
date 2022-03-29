7c08ac0f1200ac685fd909f6f8f86162f6ad710f
Mac Mini 15 <macmini15@MCM-OS-115.local>
2022-03-29 11:48:44 -0700

    while loop exercises

---
+++ exercises.js
@@ -0,0 +1,50 @@
+// Exercise 1: Write a loop that logs numbers from 1 to 9
+//-------------------
+// let i = 0;
+
+// while (i < 9) {
+//     i++;
+//     console.log(i)
+// }
+//-------------------
+//Exercise 2: Write a loop that logs the numbers 9 to 1.
+//-------------------
+// let i = 10;
+
+// while (i > 1) {
+//     i--;
+//     console.log(i)
+// }
+//-------------------
+//Exercise 3: Write a loop that returns that takes the variable $num which has a value of 10000, and divide by 2 until $num is less than 10. Log each iteration of the loop.
+//-------------------
+// var num = 10000;
+
+// while (num > 10) {
+//     num /= 2 ;
+//     console.log(num)
+// }
+//------------------
+//Exercise 4: Write a while loop that logs the number 1 through 12 but skips the number 3. You may need to use an if statement inside your loop. There is an easy way to skip a number in a loop. Try to find it.
+//------------------
+// let i = 0;
+
+// while (i < 12) {
+//     i++; 
+//     if (i == 3) {
+//             continue;
+//     }
+//     console.log(i)
+// }
+//------------------
+//Exercise 5: Write a while loop that logs the number 1 through 12 but skips the number 3 and the number 6. Use the logical OR.
+//------------------
+// let i = 0;
+
+// while (i < 12) {
+//     i++; 
+//     if (i == 3 || i == 6) {
+//             continue;
+//     }
+//     console.log(i)
+// }
